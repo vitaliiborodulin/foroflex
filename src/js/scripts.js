77af71c3898cf803b226311bc9ac43c27f11b46c
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-09-26 12:08:07 +0300

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,22 @@
+// $(function(){
+
+//   // preloader
+//   setTimeout(function() {
+//     $(".home").addClass('loaded');
+//   }, 1000);
+  
+//   $('section, .section, .fadeinitem').viewportChecker({
+//     offset: 200,
+//   });
+
+  
+// });
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelector('.footer__copy').innerHTML = '&copy; ' + new Date().getFullYear() + ' D’OROFLEX';
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
 });
-
